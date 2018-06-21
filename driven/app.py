81d2c8c719b6889d7f9be2c844ea1952c96de136
David Mougeolle <david.mougeolle@moodule.net>
2018-06-21 20:24:27 +0200

    added the official css 	modified:   driven/app.py

--- driven/app.py
+++ driven/app.py
@@ -9,8 +9,10 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-app_style = {
-    'columnCount': 2}
+app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+
+# app_style = {
+#     'columnCount': 2}
 
 #####################################################################
 # ELEMENTS
@@ -28,63 +30,71 @@ objective_elements = [(
             id=l.lower() + '-slider',
             min=1,
             max=5,
-            marks={i: str(i) for i in range(1, 6)},
-            value=5))
-    for l in objective_labels]
+            marks={i: str(i) for i in range(1, 6)} if j == 4 else {},
+            value=5-j))
+    for j, l in enumerate(objective_labels)]
 
-app.layout = html.Div([
-    html.Label('Dropdown'),
-    dcc.Dropdown(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value='MTL'
-    ),
+#####################################################################
+# LAYOUT
+#####################################################################
 
-    html.Label('Multi-Select Dropdown'),
-    dcc.Dropdown(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value=['MTL', 'SF'],
-        multi=True
-    ),
-
-    html.Label('Radio Items'),
-    dcc.RadioItems(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value='MTL'
-    ),
-
-    html.Label('Checkboxes'),
-    dcc.Checklist(
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': u'Montréal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        values=['MTL', 'SF']
-    ),
-
-    html.Label('Text Input'),
-    dcc.Input(value='MTL', type='text'),
-
-    html.Label('Slider'),
-    dcc.Slider(
-        min=0,
-        max=9,
-        marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
-        value=5,
-    ),
-], style=app_style)
+app.layout = html.Div(
+    children=(
+        [e[0] for e in objective_elements]
+        + [e[1] for e in objective_elements]))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+# [
+#     html.Label('Dropdown'),
+#     dcc.Dropdown(
+#         options=[
+#             {'label': 'New York City', 'value': 'NYC'},
+#             {'label': u'Montréal', 'value': 'MTL'},
+#             {'label': 'San Francisco', 'value': 'SF'}
+#         ],
+#         value='MTL'
+#     ),
+
+#     html.Label('Multi-Select Dropdown'),
+#     dcc.Dropdown(
+#         options=[
+#             {'label': 'New York City', 'value': 'NYC'},
+#             {'label': u'Montréal', 'value': 'MTL'},
+#             {'label': 'San Francisco', 'value': 'SF'}
+#         ],
+#         value=['MTL', 'SF'],
+#         multi=True
+#     ),
+
+#     html.Label('Radio Items'),
+#     dcc.RadioItems(
+#         options=[
+#             {'label': 'New York City', 'value': 'NYC'},
+#             {'label': u'Montréal', 'value': 'MTL'},
+#             {'label': 'San Francisco', 'value': 'SF'}
+#         ],
+#         value='MTL'
+#     ),
+
+#     html.Label('Checkboxes'),
+#     dcc.Checklist(
+#         options=[
+#             {'label': 'New York City', 'value': 'NYC'},
+#             {'label': u'Montréal', 'value': 'MTL'},
+#             {'label': 'San Francisco', 'value': 'SF'}
+#         ],
+#         values=['MTL', 'SF']
+#     ),
+
+#     html.Label('Text Input'),
+#     dcc.Input(value='MTL', type='text'),
+
+#     html.Label('Slider'),
+#     dcc.Slider(
+#         min=0,
+#         max=9,
+#         marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
+#         value=5,
+#     )]
