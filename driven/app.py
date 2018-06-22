18780e86476165cdf30aa8e076eade97c8d212e9
David Mougeolle <david.mougeolle@moodule.net>
2018-06-22 18:02:42 +0200

    basic specification form 	modified:   driven/app.py

--- driven/app.py
+++ driven/app.py
@@ -19,11 +19,45 @@ app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
 #####################################################################
 
 styles = {
-    'objective-form': {'columnCount': 2}}
+    'main-container': {'columnCount': 2},
+    'specifications-form': {},
+    'objective-form': {},
+    'product-density-input': {'display': 'none'},
+    'product-surcharge-angle-input': {'display': 'none'}}
 
 #####################################################################
 # SPECIFICATIONS FORM
 #####################################################################
+
+specification_form_rows = [
+    [
+        html.Label('Delta-x', htmlFor='delta-x-input'),
+        dcc.Input(id='delta-x-input', value=1.0, min=0.1, step=0.1, max=1.0e5),
+        html.Abbr('m')],
+    [
+        html.Label('Delta-y', htmlFor='delta-y-input'),
+        dcc.Input(id='delta-y-input', value=0.0, min=-1.0e3, step=0.1, max=1.0e3),
+        html.Abbr('m')],
+    [
+        html.Label('Output', htmlFor='output'),
+        dcc.Input(id='output', value=10.0, min=0.1, step=0.1, max=1.0e4),
+        html.Abbr('t/h')],
+    [
+        html.Label('Product', htmlFor='product-name'),
+        dcc.Dropdown(
+            id='product-name',
+            options=[
+                {'label':'Coal', 'value':'coal'},
+                {'label':'Truc', 'value':'truc'}],
+            value='coal',
+            multi=False)]]
+
+specification_form = html.Form(
+    children=html.Fieldset(
+        children=(
+            [html.Legend('Specifications')]
+            + [html.P(children=list(r)) for r in specification_form_rows])),
+    id='specification-form')
 
 #####################################################################
 # OBJECTIVE FORM
@@ -79,8 +113,9 @@ objective_form = html.Form(
 #####################################################################
 
 app.layout = html.Div(
-    children=[objective_form],
-    id='main-container')
+    children=[specification_form, objective_form],
+    id='main-container',
+    style=styles['main-container'])
 
 #####################################################################
 # CALLBACKS
