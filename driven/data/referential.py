1cd8bc5c91f4170b1628f27e856bfed13c7aac86
David Mougeolle <david.mougeolle@moodule.net>
2018-09-06 17:55:09 +0200

    ask the db for the list of bulk materials & memoize the result 	new file:   driven/data/referential.py

---
+++ driven/data/referential.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import
+
+import sqlite3
+
+from driven._lib import *
+
+#####################################################################
+# PRODUCT DATA
+#####################################################################
+@memoize
+def bulk_material_data():
+    with sqlite3.connect('./driven/data/referential.sqlite3') as ref_db:
+        cursor = ref_db.cursor()
+        product_catalog = {
+            product[0]: product[1:]
+            for product in cursor.execute(
+                """select id, name, average_density, surcharge_angle
+                from bulk_material
+                order by name;""")}
+
+    return product_catalog
