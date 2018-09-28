4096f7f7c987090a856478407f36961ff21f4338
David Mougeolle <david.mougeolle@moodule.net>
2018-09-28 18:06:38 +0200

    erika crashed on my project

--- driven/app.py
+++ driven/app.py
@@ -3,17 +3,18 @@ import copy
 import math
 
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
 
 from driven._lib import *
 from driven.charts.layout import conveyor_layout_graph
 from driven.charts.navigation import navigation_graph
 from driven.charts.objectives import (
-    rating_overview_graph,
-    cost_graph,
-    safety_graph,
-    reliability_graph,
-    stability_graph)
+    make_overview_figure,
+    make_cost_figure,
+    make_safety_figure,
+    make_reliability_figure,
+    make_stability_figure)
 from driven.data.referential import bulk_material_data
 from driven.forms.design import local_design_form
 from driven.forms.objectives import objectives_form
@@ -29,13 +30,13 @@ from driven.frame import (
 # THIRD PARTY
 #####################################################################
 
-mapbox_access_token = ''
+mapbox_access_token = 'pk.eyJ1IjoibW9vZHVsZSIsImEiOiJjam1lcW1qNW0wcG9rM3dsbHY2N2ZyZ29iIn0.TkBXhBxfadbKKkRH7320Ng'
 
 #####################################################################
 # APP
 #####################################################################
 
-app = dash.Dash('Industrious')
+app = dash.Dash('Driven')
 app.css.append_css({'external_url': 'https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css'})
 
 server = app.server
@@ -55,18 +56,18 @@ app.layout = html.Div(
             className='row'),
         html.Div(
             children=[
-                navigation_graph(),
-                local_design_form()],
-            className='row'),
-        html.Div(
-            children=[
-                safety_graph(),
-                cost_graph(),
-                reliability_graph()],
-            className='row'),
-        html.Div(
-            children=[
-                conveyor_layout_graph()],
+                html.Div(
+                    children=[dcc.Graph(id='safety_graph')],
+                    className='four columns',
+                    style={'margin-top': '10'}),
+                html.Div(
+                    children=[dcc.Graph(id='cost_graph')],
+                    className='four columns',
+                    style={'margin-top': '10'}),
+                html.Div(
+                    children=[dcc.Graph(id='reliability_graph')],
+                    className='four columns',
+                    style={'margin-top': '10'}),],
             className='row')],
     id='main_container',
     className='ten columns offset-by-one')
@@ -75,7 +76,7 @@ widget_layout = dict(
     autosize=True,
     height=500,
     font=dict(color='#CCCCCC'),
-    titlefont=dict(color='#CCCCCC', size='14'),
+    titlefont=dict(color='#CCCCCC', size=14),
     margin=dict(
         l=35,
         r=35,
@@ -99,7 +100,7 @@ widget_layout = dict(
 )
 
 #####################################################################
-# EVENTS
+# UPDATE SPECIFICATIONS
 #####################################################################
 
 @app.callback(
@@ -136,6 +137,39 @@ def update_surcharge_angle_input(product_id):
     if product:
         return 180.0 * product[2] / math.pi
 
+###############################################################################
+# UPDATE GRAPHS
+###############################################################################
+
+@app.callback(
+    dash.dependencies.Output('safety_graph', 'figure'),
+    [
+        dash.dependencies.Input('total_delta_x_input', 'value'),
+        dash.dependencies.Input('total_delta_y_input', 'value'),
+        dash.dependencies.Input('output_input', 'value'),
+        dash.dependencies.Input('product_name_input', 'value')])
+def update_safety_graph(x, y, q, p):
+    return make_safety_figure(widget_layout)
+
+@app.callback(
+    dash.dependencies.Output('cost_graph', 'figure'),
+    [
+        dash.dependencies.Input('total_delta_x_input', 'value'),
+        dash.dependencies.Input('total_delta_y_input', 'value'),
+        dash.dependencies.Input('output_input', 'value'),
+        dash.dependencies.Input('product_name_input', 'value')])
+def update_safety_graph(x, y, q, p):
+    return make_cost_figure(widget_layout)
+
+@app.callback(
+    dash.dependencies.Output('reliability_graph', 'figure'),
+    [
+        dash.dependencies.Input('total_delta_x_input', 'value'),
+        dash.dependencies.Input('total_delta_y_input', 'value'),
+        dash.dependencies.Input('output_input', 'value'),
+        dash.dependencies.Input('product_name_input', 'value')])
+def update_safety_graph(x, y, q, p):
+    return make_reliability_figure(widget_layout)
 
 #####################################################################
 # SERVER
