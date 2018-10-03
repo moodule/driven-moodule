e9d7edd4f72cabc0ac5f156c3e569c5eb0a42dfd
David Mougeolle <david.mougeolle@moodule.net>
2018-10-03 18:59:09 +0200

    only display the results on the user inputs & give details by splitting the bars into stacks

--- driven/charts/objectives.py
+++ driven/charts/objectives.py
@@ -68,70 +68,26 @@ def make_cost_figure(layout):
 # SAFETY
 #####################################################################
 def make_safety_figure(layout):
-    x = ['Snatching', 'Falling', 'Cutting']
-    y = [760, 400, 4]
-    y2 = [230, 120, 2]
-    data = [
-        go.Bar(
-            name='User Design',
-            x=x,
-            y=y,
-            textposition = 'auto',
-            marker=dict(
-                color='rgb(158,202,225)',
-                line=dict(
-                    color='rgb(8,48,107)',
-                    width=1.5),),
-            opacity=0.6),
-        go.Bar(
-            name='Optimal Design',
-            x=x,
-            y=y2,
-            textposition = 'auto',
-            marker=dict(
-                color='rgb(58,200,225)',
-                line=dict(
-                    color='rgb(8,48,107)',
-                    width=1.5),),
-            opacity=0.6)]
-
     safety_layout = copy.deepcopy(layout)
     safety_layout['title'] = 'Safety Concerns'
-    safety_layout['barmode'] = 'group'
+    safety_layout['barmode'] = 'stack'
     safety_layout['showlegend'] = True
     safety_layout['margin'] = go.layout.Margin(l=40, r=40, t=40, b=40)
 
-    return dict(data=data, layout=go.Layout(safety_layout))
+    return dict(data=_random_safety_data(), layout=go.Layout(safety_layout))
 
 #####################################################################
 # RELIABILITY
 #####################################################################
 def make_reliability_figure(layout):
-    x = ['Belt RMBT', 'Idlers RMBT/Wear', 'Pulleys RMBT/Wear', 'Laggings RMBT/Wear', 'Splice RMBT']
-    y1 = [110, 40, 40, 150, 130]
-    y2 = [80, 60, 50, 70, 90]
-    data = [
-        go.Bar(
-            name='User Design',
-            x=x,
-            y=y1,
-            textposition = 'auto',
-            opacity=0.6),
-        go.Bar(
-            name='Optimal Design',
-            x=x,
-            y=y2,
-            textposition = 'auto',
-            opacity=0.6)]
-
     reliability_layout = copy.deepcopy(layout)
-    reliability_layout['title'] = 'Min/Max/Average Wear'
-    reliability_layout['barmode'] = 'group'
-    reliability_layout['yaxis'] = dict(title='RMBT (%)')
+    reliability_layout['title'] = 'Components Wear'
+    reliability_layout['barmode'] = 'relative'
+    reliability_layout['yaxis'] = dict(title='Constraints / Strength (%)')
     reliability_layout['showlegend'] = False
     reliability_layout['margin'] = go.layout.Margin(l=40, r=40, t=40, b=40)
 
-    return dict(data=data, layout=go.Layout(reliability_layout))
+    return dict(data=_random_reliability_data(), layout=go.Layout(reliability_layout))
 
 #####################################################################
 # Stability
