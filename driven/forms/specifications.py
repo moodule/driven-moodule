49eab46cf4d258d27aa9f6887eddf7cfac4e41d0
David Mougeolle <david.mougeolle@moodule.net>
2018-10-03 19:00:48 +0200

    minor tweaks on the position & size of input fields

--- driven/forms/specifications.py
+++ driven/forms/specifications.py
@@ -22,7 +22,8 @@ def _product_form():
                 children=[
                     html.Label(
                         'Transporting',
-                        className='two columns'),
+                        className='two columns',
+                        style={'position':'relative','text-align':'right', 'height':'100px', 'padding-right':'10px'}),
                     dcc.Dropdown(
                         id='product_name_input',
                         clearable=False,
@@ -32,10 +33,12 @@ def _product_form():
                         options=[
                             {'label': product[0], 'value': pid}
                             for pid, product in bulk_material_data().items()],
-                        className='three columns'),
+                        className='three columns',
+                        style={'position':'relative','top':'50%'}),
                     html.Label(
-                        'At',
-                        className='one columns'),
+                        'at',
+                        className='one columns',
+                        style={'text-align':'center'}),
                     dcc.RangeSlider(
                         id='output_input',
                         allowCross=False,
@@ -47,14 +50,14 @@ def _product_form():
                     html.Label(
                         '',
                         id='output_label',
-                        className='two columns')],
+                        className='two columns',
+                        style={'text-align':'center'})],
                     className='row'),
             html.Div(
                 children=[
                     html.Label(
                         'Bulk Density (t/m³)',
-                        id='product_density_label',
-                        htmlFor='product_density_input'),
+                        id='product_density_label'),
                     dcc.Input(
                         id='product_density_input',
                         type='number',
@@ -64,8 +67,7 @@ def _product_form():
                         max=1.0e2),
                     html.Label(
                         'Surcharge Angle (°)',
-                        id='product_surcharge_angle_label',
-                        htmlFor='product_surcharge_angle_input'),
+                        id='product_surcharge_angle_label'),
                     dcc.Input(
                         id='product_surcharge_angle_input',
                         type='number',
@@ -77,44 +79,6 @@ def _product_form():
         className='three rows')
 
 #####################################################################
-# GEOMETRY
-#####################################################################
-def _geometry_form():
-    return html.Div(
-        children=[
-            html.Label(
-                'Over',
-                className='two columns'),
-            dcc.RangeSlider(
-                id='total_delta_x_input',
-                allowCross=False,
-                value=[1.0, 1.0],
-                min=0.0,
-                step=0.1,
-                max=4.0,
-                marks={0.0:'1 m', 1.0:'10 m', 2.0:'100 m', 3.0:'1 km', 4.0:'10 km'},
-                className='four columns'),
-            html.Label(
-                '',
-                id='total_delta_x_label',
-                className='two columns'),
-            dcc.RangeSlider(
-                id='total_delta_y_input',
-                allowCross=False,
-                value=[0.0, 0.0],
-                min=-1.0e2,
-                step=0.5,
-                max=1.0e2,
-                vertical=True,
-                marks={-100.0:'-100 m', -10.0:'-10 m', 0.0:'0', 10.0:'10 m', 100.0:'100 m'},
-                className='two columns'),
-            html.Label(
-                '',
-                id='total_delta_y_label',
-                className='two columns')],
-        className='three rows')
-
-#####################################################################
 # META
 #####################################################################
 def _priority_form():
@@ -122,7 +86,8 @@ def _priority_form():
         children=[
             html.Label(
                 'With',
-                className='two columns'),
+                className='two columns',
+                style={'text-align':'right'}),
             dcc.Dropdown(
                 id='priority_input',
                 clearable=False,
@@ -133,10 +98,11 @@ def _priority_form():
                     {'label': 'Cost', 'value': 0},
                     {'label': 'Reliability', 'value': 1},
                     {'label': 'Safety', 'value': 2}],
-                className='two columns'),
+                className='three columns'),
             html.Label(
                 'as priority',
-                className='two columns')],
+                className='two columns',
+                style={'text-align':'center'})],
         className='three rows')
 
 def _scenario_form():
@@ -144,7 +110,8 @@ def _scenario_form():
         children=[
             html.Label(
                 'Considering',
-                className='two columns'),
+                className='two columns',
+                style={'text-align':'right'}),
             dcc.Dropdown(
                 id='scenario_input',
                 clearable=False,
@@ -156,10 +123,52 @@ def _scenario_form():
                     {'label': 'The best', 'value': 1},
                     {'label': 'The average', 'value': 2},
                     {'label': 'The worst', 'value': 3}],
-                className='two columns'),
+                className='three columns'),
             html.Label(
                 'case scenario',
-                className='two columns')],
+                className='two columns',
+                style={'text-align':'center'})],
+        className='three rows')
+
+#####################################################################
+# GEOMETRY
+#####################################################################
+def _geometry_form():
+    return html.Div(
+        children=[
+            html.Label(
+                'Over',
+                className='two columns',
+                style={'text-align':'right'}),
+            dcc.RangeSlider(
+                id='total_delta_x_input',
+                allowCross=False,
+                value=[1.0, 1.0],
+                min=0.0,
+                step=0.1,
+                max=4.0,
+                marks={0.0:'1 m', 1.0:'10 m', 2.0:'100 m', 3.0:'1 km', 4.0:'10 km'},
+                className='three columns'),
+            html.Label(
+                '',
+                id='total_delta_x_label',
+                className='three columns',
+                style={'text-align':'center'}),
+            dcc.RangeSlider(
+                id='total_delta_y_input',
+                allowCross=False,
+                value=[0.0, 0.0],
+                min=-1.0e2,
+                step=0.5,
+                max=1.0e2,
+                vertical=True,
+                marks={-100.0:'-100 m', -10.0:'-10 m', 0.0:'0', 10.0:'10 m', 100.0:'100 m'},
+                className='one columns'),
+            html.Label(
+                '',
+                id='total_delta_y_label',
+                className='three columns',
+                style={'text-align':'center'})],
         className='three rows')
 
 #####################################################################
@@ -192,8 +201,8 @@ def specifications_form():
         id='specifications_form',
         children=[
             _product_form(),
-            _geometry_form(),
             _priority_form(),
-            _scenario_form()],
+            _scenario_form(),
+            _geometry_form()],
         className='eight columns',
         style={'height':'300px'})
