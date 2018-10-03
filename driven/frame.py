e058d1565cc7f07aca4ddadd9318404ee3ba6dfa
David Mougeolle <david.mougeolle@moodule.net>
2018-10-03 19:01:20 +0200

    merged the cost summaries

--- driven/frame.py
+++ driven/frame.py
@@ -23,7 +23,7 @@ def make_logo():
             'position': 'relative'})
 
 def update_title():
-    return 'Belt Conveyor - Design'
+    return 'New Belt Conveyor - Design'
 
 #####################################################################
 # SUMMARY
@@ -33,7 +33,7 @@ def make_risk_summary_container():
     return html.Div(
         id='risk_summary_container',
         children=[
-            html.H5(id='risk_summary_text'),
+            html.H5(id='risk_summary_text', style={'text-align':'left'}),
             html.Span(id='risk_summary_tooltip', className='tooltiptext')],
         className='two columns tooltip')
 
@@ -41,31 +41,15 @@ def make_cost_summary_container():
     return html.Div(
         id='cost_summary_container',
         children=[
-            html.Div(
-                id='buying_cost_summary_container',
-                children=[
-                    html.H5(id='buying_cost_summary_text'),
-                    html.Span(id='buying_cost_summary_tooltip', className='tooltiptext')],
-                className='four columns tooltip'),
-            html.Div(
-                id='operating_cost_summary_container',
-                children=[
-                    html.H5(id='operating_cost_summary_text'),
-                    html.Span(id='operating_cost_summary_tooltip', className='tooltiptext')],
-                className='four columns tooltip'),
-            html.Div(
-                id='maintenance_cost_summary_container',
-                children=[
-                    html.H5(id='maintenance_cost_summary_text'),
-                    html.Span(id='maintenance_cost_summary_tooltip', className='tooltiptext')],
-                className='four columns tooltip')],
-            className='eight columns')
+            html.H5(id='cost_summary_text', style={'text-align':'center'}),
+            html.Span(id='cost_summary_tooltip', className='tooltiptext')],
+        className='eight columns tooltip')
 
 def make_reliability_summary_container():
     return html.Div(
         id='reliability_summary_container',
         children=[
-            html.H5(id='reliability_summary_text'),
+            html.H5(id='reliability_summary_text', style={'text-align':'right'}),
             html.Span(id='reliability_summary_tooltip', className='tooltiptext')],
         className='two columns tooltip')
 
@@ -75,23 +59,11 @@ def update_risk_summary_text():
 def update_risk_summary_tooltip():
     return 'Can be lowered to 560'
 
-def update_buying_cost_summary_text():
-    return 'Buying : 10 k€'
+def update_cost_summary_text():
+    return 'Buying : 10 k€ | Operating : 4 k€/year | Maintenance : 2 k€/year'
 
-def update_buying_cost_summary_tooltip():
-    return 'Can be lowered to 7 k€'
-
-def update_operating_cost_summary_text():
-    return 'Operating : 4 k€/year'
-
-def update_operating_cost_summary_tooltip():
-    return 'Can be lowered to 3.5 k€/year'
-
-def update_maintenance_cost_summary_text():
-    return 'Maintenance : 2 k€/year'
-
-def update_maintenance_cost_summary_tooltip():
-    return 'Can be lowered to 1.5 k€/year'
+def update_cost_summary_tooltip():
+    return 'Can be lowered to 7 k€ | 3.5 k€/year | 1.5 k€/year'
 
 def update_reliability_summary_text():
     return 'Wear : Low'
